55a2d1467ae7a89c258689f3d83a105797c55ca3
Dries Cruyskens <driesc@Dries-Cruyskens.local>
2017-11-07 18:04:43 +0100

    Init

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<title></title>
+	<meta charset="utf-8">
+    <!-- build:css css/style.min.css -->
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+    <!-- endbuild -->
+    <!-- build:js js/main.min.js -->
+    <script type="text/javascript" src="js/script.js"></script>
+    <!-- endbuild -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="img/favicon.ico">
+</head>
+<body>
+    <?php
+       echo "<p>Hello world!</p>";
+       phpinfo();
+    ?>
+</body>
+</html>
