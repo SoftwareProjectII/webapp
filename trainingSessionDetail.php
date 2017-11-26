3a5902bfec1cb31657fe42adb838890ad7d51ae6
Dries Cruyskens <driesc@Dries-Cruyskens.local>
2017-11-26 17:29:50 +0100

    added correct html & css styling

--- trainingSessionDetail.php
+++ trainingSessionDetail.php
@@ -11,8 +11,6 @@ require_once 'templates/head.php';
 
 session_start();
 
-$TS;
-
 //if sign in button pushed => compile data and post to dataservice
 //TODO: change to function like in trainingsessiondetail?
 if (isset($_GET["trainingSessionId"]) && isset($_SESSION["user"]["userId"]) && isset($_GET["signin"])) {
@@ -100,127 +98,140 @@ if (isset($_GET["trainingSessionId"]) && isset($_SESSION["user"]["userId"])) {
 ?>
 <body>
 <?php require_once 'templates/navigation.php';?>
-<section>
-    <?php if($TS["cancelled"] == 1) {
-
-    } else if(!checkForId($TS["trainingSessionId"], $_SESSION["userTrainingSessions"]) || ($status["iscancelled"] == "true" && $status["isapproved"] =="false")) { // not in followingtrainings -> sign in button -> in followingstrainings aproved & iscancelled on false
-        ?>
-        <form>
-            <input type="hidden" name="trainingSessionId" value="<?= $TS["trainingSessionId"] ?>"/>
-            <input type="hidden" name="signin" value="true"/>
-            <button name="sign in">
-                Sign in
-            </button>
-        </form>
-        <?php
-    } else if ($status["iscancelled"] == "false" && $status["isapproved"] == "false") { // approved & iscancelled false (manager must approve) -> cancel button -> iscancelled on true
-        ?>
-        <form>
-            <input type="hidden" name="trainingSessionId" value="<?= $TS["trainingSessionId"] ?>"/>
-            <input type="hidden" name="cancelsignin" value="true"/>
-            <button name="sign in">
-                (awaiting confirmation) Cancel sign in
-            </button>
-        </form>
-        <?php
-    } else if ($status["iscancelled"] == "false" && $status["isapproved"] == "true") { // approved = true, cancelled = false --> manager approved --> sign out button --> iscancelled = true
-        ?>
-        <form>
-            <input type="hidden" name="trainingSessionId" value="<?= $TS["trainingSessionId"] ?>"/>
-            <input type="hidden" name="signout" value="true"/>
-            <button name="sign out">
-                sign out
-            </button>
-        </form>
-        <?php
-    } else if ($status["iscancelled"] == "true" && $status["isapproved"] == "true") { // approved = true & iscancelled true -> signed out
-        ?>
-        <form>
-            <input type="hidden" name="trainingSessionId" value="<?= $TS["trainingSessionId"] ?>"/>
-            <input type="hidden" name="cancelsignout" value="true"/>
-            <button name="signed out" disabled>
-                (awaiting confirmation) Cancel signed out
-            </button>
-        </form>
-        <?php
-    }
-    ?>
-
-    <h6>
-        Training:
-    </h6>
-    <p>
-        <?php
-            echo $TS["trainingsinfo"]["name"];
-            if ($TS["cancelled"] == 1) {
-                ?>
-                <mark> Cancelled!</mark>
+<div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <!-- TODO: back to where you came from (breadcrumb variable)-->
+                <a class="btn btn-primary" role="button" href="#"> <i class="icon ion-android-arrow-back"></i> BACK</a>
                 <?php
-            }
-        ?>
-    </p>
-    <h6>
-        Info:
-    </h6>
-    <p>
-        <?= $TS["trainingsinfo"]["infogeneral"];?>
-    </p>
-    <h6>
-        Date:
-    </h6>
-    <p>
-        <?php
-            $date = new DateTime($TS["date"]);
-            echo $date->format('d M Y');
-        ?>
-    </p>
-    <h6>
-        Time:
-    </h6>
-    <p>
-        <?php
-            $start = new DateTime($TS["startHour"]);
-            $end = new DateTime($TS["endHour"]);
-            echo $start->format('H:i') . ' - ' . $end->format('H:i');
-        ?>
-    </p>
-    <h6>
-        Price:
-    </h6>
-    <p>
-        <?= $TS["trainingsinfo"]["price"];?>
-    </p>
-    <h6>
-        Payment:
-    </h6>
-    <p>
-        <?= $TS["trainingsinfo"]["infopayment"];?>
-    </p>
-    <h6>
-        Exam:
-    </h6>
-    <p>
-        <?= $TS["trainingsinfo"]["infoexam"];?>
-    </p>
-    <h6>
-        Teacher:
-    </h6>
-    <p>
-        <?= $teacher["firstname"] . $teacher["lastname"] . $teacher["phonenumber"] . $teacher["email"];?>
-    </p>
-    <h6>
-        Location:
-    </h6>
-    <p>
-        <?= $address["locality"] . $address["streetaddress"];?>
-    </p>
-    <iframe
-            width="600"
-            height="450"
-            frameborder="0" style="border:0"
-            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyC_MyMlEb59Fb8IBPy_0hHm4FGP4r8nYxo
-    &q=<?= urlencode($address["country"]) . "+" . urlencode($address["locality"]) . "+" . urlencode($address["streetaddress"]);
-            ?>">
-    </iframe>
-</section>
+                if($TS["cancelled"] == 1) {
+
+                } else if(!checkForId($TS["trainingSessionId"], $_SESSION["userTrainingSessions"]) || ($status["iscancelled"] == "true" && $status["isapproved"] =="false")) { // not in followingtrainings -> sign in button -> in followingstrainings aproved & iscancelled on false
+                    ?>
+                    <form>
+                        <input type="hidden" name="trainingSessionId" value="<?= $TS["trainingSessionId"] ?>"/>
+                        <input type="hidden" name="signin" value="true"/>
+                        <button class="btn btn-primary float-right" name="sign in">
+                            Sign in
+                        </button>
+                    </form>
+                    <?php
+                } else if ($status["iscancelled"] == "false" && $status["isapproved"] == "false") { // approved & iscancelled false (manager must approve) -> cancel button -> iscancelled on true
+                    ?>
+                    <form>
+                        <input type="hidden" name="trainingSessionId" value="<?= $TS["trainingSessionId"] ?>"/>
+                        <input type="hidden" name="cancelsignin" value="true"/>
+                        <button class="btn btn-primary float-right" name="sign in">
+                            (awaiting confirmation) Cancel sign in
+                        </button>
+                    </form>
+                    <?php
+                } else if ($status["iscancelled"] == "false" && $status["isapproved"] == "true") { // approved = true, cancelled = false --> manager approved --> sign out button --> iscancelled = true
+                    ?>
+                    <form>
+                        <input type="hidden" name="trainingSessionId" value="<?= $TS["trainingSessionId"] ?>"/>
+                        <input type="hidden" name="signout" value="true"/>
+                        <button class="btn btn-primary float-right" name="sign out">
+                            sign out
+                        </button>
+                    </form>
+                    <?php
+                } else if ($status["iscancelled"] == "true" && $status["isapproved"] == "true") { // approved = true & iscancelled true -> signed out
+                    ?>
+                    <form>
+                        <input type="hidden" name="trainingSessionId" value="<?= $TS["trainingSessionId"] ?>"/>
+                        <input type="hidden" name="cancelsignout" value="true"/>
+                        <button class="btn btn-primary float-right" name="signed out" disabled>
+                            (awaiting confirmation) Cancel signed out
+                        </button>
+                    </form>
+                    <?php
+                }
+                ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <h3>Training Info
+                </h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="trainingsinfoborder">
+                    <h5>
+                        <?php
+                        echo $TS["trainingsinfo"]["name"];
+                        if ($TS["cancelled"] == 1) {
+                            ?>
+                            <mark> Cancelled!</mark>
+                            <?php
+                        }
+                        ?>
+                    </h5>
+                    <p>
+                        <?= $TS["trainingsinfo"]["infogeneral"];?>
+                    </p>
+                    <h5>Date - Hour</h5>
+                    <p>
+                        <?php
+                        $date = new DateTime($TS["date"]);
+                        echo $date->format('d M Y');
+                        ?> -
+                        <?php
+                        $start = new DateTime($TS["startHour"]);
+                        $end = new DateTime($TS["endHour"]);
+                        echo $start->format('H:i') . '-' . $end->format('H:i');
+                        ?>
+                    </p>
+                    <h5>
+                        Price:
+                    </h5>
+                    <p>
+                        <?= $TS["trainingsinfo"]["price"];?>
+                    </p>
+                    <h5>
+                        Payment:
+                    </h5>
+                    <p>
+                        <?= $TS["trainingsinfo"]["infopayment"];?>
+                    </p>
+                    <h5>
+                        Exam:
+                    </h5>
+                    <p>
+                        <?= $TS["trainingsinfo"]["infoexam"];?>
+                    </p>
+                    <h5>
+                        Teacher:
+                    </h5>
+                    <p>
+                        <?= $teacher["firstname"] . $teacher["lastname"] . $teacher["phonenumber"] . $teacher["email"];?>
+                    </p>
+                    <h5>
+                        Location:
+                    </h5>
+                    <p>
+                        <?= $address["locality"] . $address["streetaddress"];?>
+                    </p>
+                    <div class="col">
+                        <iframe
+                                width="600"
+                                height="450"
+                                frameborder="0" style="border:0"
+                                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyC_MyMlEb59Fb8IBPy_0hHm4FGP4r8nYxo&amp;q=<?= urlencode($address["country"]) . "+" . urlencode($address["locality"]) . "+" . urlencode($address["streetaddress"]);
+                                ?>">
+                        </iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <a class="btn btn-primary" role="button" href="#"> <i class="icon ion-android-arrow-back"></i> BACK</a>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
