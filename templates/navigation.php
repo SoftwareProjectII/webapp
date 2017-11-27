fa725f5d4666239e23858fcd3542697219e5f585
Dries Cruyskens <driesc@Dries-Cruyskens.local>
2017-11-27 11:29:26 +0100

    minor change to navigation template

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,10 +1,11 @@
+<!-- TODO: add active class to navigation link when on that page-->
 <div>
     <nav class="navbar navbar-light navbar-expand-md navigation-clean">
         <div class="container"><a class="navbar-brand" href="#">Company Name</a>
             <button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navcol-1">
                 <ul class="nav navbar-nav ml-auto">
-                    <li class="nav-item" role="presentation"><a class="nav-link active" href="availabletraining.php">Available Training</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="availabletraining.php">Available Training</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link" href="registeredtraining.php">Registered Training</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link" href="mytraining.php">My Training</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link" href="myconfirmations.php">My Confirmations</a></li>
